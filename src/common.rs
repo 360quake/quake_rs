69e4ace2afba6d57e3ca00a871efba9be54f59c9
dengqiang1 <dengqiang1@360.cn>
2022-09-16 09:56:27 +0800

    添加批量查询的功能，读取txt中查询语句，将结果发回到新的txt中，并更新readme

--- src/common.rs
+++ src/common.rs
@@ -48,7 +48,7 @@ pub struct ArgParse;
 impl ArgParse {
     pub fn parse() {
         let matches = App::new("Quake Command-Line Application")
-            .version("2.0.2")
+            .version("2.2")
             .author("Author: 360 Quake Team  <quake@360.cn>")
             .about("Dose awesome things.")
             .subcommand(
